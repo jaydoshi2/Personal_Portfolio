be61e53ab84694102aad7d6e070e41fbd92e2c19
Jay D Doshi <workingjay2010@gmail.com>
2025-05-28 10:26:25 -0400

    Refactor Hero and Computers components for improved structure and consistency

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,62 @@
+// import { motion } from "framer-motion";
+
+// import { styles } from "../styles";
+// import { ComputersCanvas } from "./canvas/index.js";
+// import Position from "./Position";
+
+// const Hero = () => {
+//   return (
+//     <section className="relative w-full h-auto sm:h-screen mx-auto">
+//       <div
+//         className={`pt-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+//       >
+
+//         <div className='flex flex-col justify-center items-center mt-5'>
+//           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+//           <div className='w-1 sm:h-80 h-40 violet-gradient' />
+//         </div>
+
+//         <div>
+//           <h1 className={`${styles.heroHeadText} text-white`}>
+//             Hi, I'm <span className='text-[#915EFF]'>Jay D Doshi</span>
+//           </h1>
+//           <Position />
+//         </div>
+//       </div>
+
+
+
+//       <div className='hidden sm:flex absolute xs:bottom-10 bottom-32 w-full justify-center items-center'>
+//         <a href='#about'>
+//           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+//             <motion.div
+//               animate={{ y: [0, 24, 0] }}
+//               transition={{ duration: 1.5, repeat: Infinity, repeatType: "loop" }}
+//               className='w-3 h-3 rounded-full bg-secondary mb-1'
+//             />
+//           </div>
+//         </a>
+//       </div>
+//     </section>
+//   );
+// };
+
+
+// export default Hero;
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas/index.js";
+import { ComputersCanvas } from "./canvas";
 import Position from "./Position";
+
 
 const Hero = () => {
   return (
     <section className="relative w-full h-auto sm:h-screen mx-auto">
-      <div
-        className={`pt-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-      >
-
+<div
+  // className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+  className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
@@ -18,14 +64,14 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Jay D Doshi</span>
+            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
           </h1>
           <Position />
         </div>
       </div>
 
-      {/* Hide on mobile */}
-      <div className="hidden sm:block">
+      {/* //       Hide on mobile */}
+   <div className="hidden sm:flex w-full justify-center items-center h-[600px] mt-4 mb-2">
         <ComputersCanvas />
       </div>
 
@@ -43,6 +89,5 @@ const Hero = () => {
     </section>
   );
 };
-
 
 export default Hero;
